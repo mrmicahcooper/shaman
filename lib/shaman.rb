04a726dcd80357be061c8f8f135ae46cf40ed841
Micah Cooper <dev+mrmicahcooper@hashrocket.com>
2014-08-04 11:05:13 -0400

    Add gem with specs

--- lib/shaman.rb
+++ lib/shaman.rb
@@ -1,5 +1,35 @@
 require "shaman/version"
+require 'oj'
 
-module Shaman
-  # Your code goes here...
+class Shaman
+  attr_reader :body
+
+  def initialize(body)
+    @body = body.to_s
+  end
+
+  def sha
+    Digest::MD5.hexdigest(prepped_body.to_s)
+  end
+
+  def valid_json_body?
+    parsed_body.class == Hash
+  end
+
+  private
+
+  def prepped_body
+    if valid_json_body?
+      Hash[parsed_body.sort]
+    else
+      parsed_body
+    end
+  end
+
+  def parsed_body
+    @parsed_body ||= Oj.load(body)
+  rescue Oj::ParseError
+    body
+  end
+
 end
--- lib/shaman/version.rb
+++ lib/shaman/version.rb
@@ -1,3 +1,3 @@
-module Shaman
+class Shaman
   VERSION = "0.0.1"
 end
